a02075395a7e10f46cff8855982ccc68a0e99d2f
Kacper Pruszyński <kacper.pruszynski@movecloser.pl>
2020-10-24 23:27:20 +0200

    Add base extension files.

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+/**
+ * Cutter app workers.
+ * Work with browser websites.
+ *
+ * @author Kacper Pruszyński (plumthedev) <kacper.pruszynski99@gmail.com>
+ * @package Cutter
+ */
+
+import "@services/cutter/worker";
---
+++ src/background.ts
@@ -0,0 +1,9 @@
+/**
+ * Cutter background script.
+ * Handle all commands.
+ *
+ * @author Kacper Pruszyński (plumthedev) <kacper.pruszynski99@gmail.com>
+ * @package Cutter
+ */
+
+import './core/commands/handler';
