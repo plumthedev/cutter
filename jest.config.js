283f00433a47e0b0b1e673463d12a8d3f0834a3e
Kacper Pruszyński <kacper.pruszynski@movecloser.pl>
2020-11-01 19:56:09 +0100

    Fix module names mapping in jest configuration.

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,12 @@
-const {pathsToModuleNameMapper} = require('ts-jest/utils');
-const {compilerOptions} = require('./tsconfig.json');
-
 module.exports = {
     preset: 'ts-jest/presets/js-with-babel',
     "roots": [
         "tests"
     ],
-    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths)
+    moduleNameMapper: {
+        '^@config/(.*)$': '<rootDir>/src/config/$1',
+        '^@core/(.*)$': '<rootDir>/src/core/$1',
+        '^@services/(.*)$': '<rootDir>/src/services/$1',
+        '^@tests/(.*)$': '<rootDir>/tests/$1',
+    }
 };
